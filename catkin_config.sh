43aaa7688bec99f1190044c17a2e1531277c51ea
李选聪 <870880194@qq.com>
2021-06-25 14:55:16 +0800

    update catkin_config.sh

--- catkin_config.sh
+++ catkin_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 dir=$(cd $(dirname "${BASH_SOURCE[0]:-$0}") && pwd)
-ros_workspace=$(catkin locate)
-catkin config $@ --cmake-args "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
+ros_workspace=$(echo $ROS_WORKSPACE)
+cd $ros_workspace && catkin config $@ --cmake-args "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
 cp -f "${dir}/.ycm_extra_conf.py" "${ros_workspace}/.ycm_extra_conf.py"
